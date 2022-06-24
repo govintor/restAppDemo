b463cc9d842225f6891dab5e10225c6012fcdd21
George Rodriguez <govintor@icloud.com>
2022-06-24 17:57:05 -0400

    addind middleware

--- server.js
+++ server.js
@@ -16,7 +16,20 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)
         collection = db.collection('restaurants')
     })
+// middlewares
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
 
+app.get('/', async (request, response) => {
+    try {
+        response.render('index.ejs')
+    } catch (error) {
+        response.status(500).send({message: error.message})
+    }
+})
 
     //PORT = 8000
 app.listen(process.env.PORT || PORT, () => {
