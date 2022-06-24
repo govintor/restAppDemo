c6cfd608fdb6f1ab25a05a2eeb20be5d5a388fff
George Rodriguez <govintor@icloud.com>
2022-06-24 17:53:24 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
+const PORT = 8000
+
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'sample_restaurants',
+    collection
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log(`Connected to Database`)
+        db = client.db(dbName)
+        collection = db.collection('restaurants')
+    })
+
+
+    //PORT = 8000
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port`)
+})
